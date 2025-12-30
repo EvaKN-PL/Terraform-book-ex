ef8e762b60eb838d5150dbffe8a039872e7b32bb
Ewa Nizioł <kiepas.ewa@gmail.com>
2025-12-30 11:16:04 +0100

    drugi commit poprawka w db

--- cwiczenie1/db.tf
+++ cwiczenie1/db.tf
@@ -1,7 +1,7 @@
 resource "aws_dynamodb_table" "terraform_locks" {
 
     name = "terraform-up-and-run-running-locks"
-    billing_mode = "PAY-PER-REQUEST"
+    billing_mode = "PAY_PER_REQUEST"
     hash_key = "LockID"
     attribute {
       name = "LockID"
