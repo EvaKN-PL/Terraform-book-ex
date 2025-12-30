6cd9c2d2151f628660f11ffefb6c9819261b2995
Ewa Nizioł <kiepas.ewa@gmail.com>
2025-12-30 15:07:45 +0100

    usuniecie lifecycle

--- cwiczenie1/main.tf
+++ cwiczenie1/main.tf
@@ -18,19 +18,9 @@ provider "aws" {
   
 }
 
-resource "aws_instance" "example" {
-    ami = "ami-015f3aa67b494b27e"
-    instance_type = "t2.micro"
-  
-}
-
 resource "aws_s3_bucket" "my_bucket" {
     bucket = "bucket-versioning-tfstate-running"
 
-    lifecycle {
-      prevent_destroy = true
-    }
-  
 }
 
 
