9147291e1a7bbef0e1602852a78c83fcd918b569
Ewa Nizioł <kiepas.ewa@gmail.com>
2025-12-31 15:02:50 +0100

    zmiany

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  backend "s3" {
+    bucket = "bucket-versioning-tfstate-running"
+    key = "services/webserver-cluster/terraform.tfstate"
+    region = "eu-central-1"
+    dynamodb_table = "terraform-up-and-run-running-locks"
+    encrypt = true
+    
+  }
+}
+
+
+
 provider "aws" {
     region = "eu-central-1"
   
