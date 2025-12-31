23a8f4c3c13526e6ace5a72baba5f7827e88a3e1
Ewa Nizioł <kiepas.ewa@gmail.com>
2025-12-31 15:57:28 +0100

    aktualizacja

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -1,15 +1,3 @@
-
-terraform {
-  backend "s3" {
-    bucket = "bucket-versioning-tfstate-running"
-    key = "stage/services/webserver-cluster/terraform.tfstate"
-    region = "eu-central-1"
-    dynamodb_table = "terraform-up-and-run-running-locks"
-    encrypt = true
-    
-  }
-}
-
 resource "aws_instance" "example" {
     ami = "ami-015f3aa67b494b27e"
     instance_type = "t2.micro"
